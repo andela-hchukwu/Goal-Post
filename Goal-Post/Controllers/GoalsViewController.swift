2ec8a235fe2a2a0a69ebb8e3b53307558d0d113a
Henry Chukwu <henry.chukwu@andela.com>
2018-05-04 15:45:13 +0100

    feature: Displaying static goal cells in UITableview

--- Goal-Post/Controllers/GoalsViewController.swift
+++ Goal-Post/Controllers/GoalsViewController.swift
@@ -19,6 +19,9 @@ class GoalsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -27,5 +30,23 @@ class GoalsViewController: UIViewController {
         
     }
     
+}
+
+extension GoalsViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell
+            else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week.", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
 }
 
