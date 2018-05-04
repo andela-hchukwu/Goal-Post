827ca7d1d013e51fea8a8c58c342b94715791c00
Henry Chukwu <henry.chukwu@andela.com>
2018-05-04 13:37:45 +0100

    feature: build goal VC

--- Goal-Post/Controllers/GoalsViewController.swift
+++ Goal-Post/Controllers/GoalsViewController.swift
@@ -15,7 +15,7 @@ class GoalsViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +23,7 @@ class GoalsViewController: UIViewController {
 
 
     @IBAction func goalButtonWasPressed(_ sender: Any) {
+        
     }
     
 }
---
+++ Goal-Post/Views/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  Goal-Post
+//
+//  Created by Henry Chukwu on 5/4/18.
+//  Copyright © 2018 Henry Chukwu. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+
+}
